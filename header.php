110eb835abcc406bf1081a13b493ce50ad5ee09c
naimish-rathod <nr553451@gmail.com>
2025-01-09 14:00:08 +0000

    Add files via upload

---
+++ header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>turboCar</title>
+	<script src="jQuery/jquery-3.7.1.min.js"></script>
+	<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+	<script src="bootstrap/js/bootstrap.bundle.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="css/font.css">
+</head>
+<body>
+<!-- Header -->
+<div class="my-container">		
+<header class="bg-col radius nav-header">
+	<nav class="navbar navbar-expand-sm">
+		<div class="container-fluid pt-3 pb-3 pe-0 ps-0">
+			<a href="#" class="navbar-brand ps-5 me-0"><img src="img/logo-7.svg"></a>
+			<ul class="navbar-nav nav-itm me-4">
+				<li class="nav-item ps-3"><a class="nav-link" href="#">Get Offer</a></li>
+				<li class="nav-item"><a class="nav-link" href="#">FAQ</a></li>
+				<li class="nav-item"><a class="nav-link" href="#">Our Story</a></li>
+				<li class="nav-item"><a class="nav-link" href="#">Blog</a></li>
+				<li class="nav-item pe-3"><a class="nav-link" href="#">88-335-3255</a></li>
+			</ul>
+			<div class="pe-5">
+				<button class="sign-up-btn">Sign Up</button>
+			</div>
+		</div>
+	</nav>
+</header>
+</div>
---
+++ index.php
@@ -0,0 +1,49 @@
+<?php 
+include 'header.php';
+?>
+<!-- Baneer section -->
+<section >
+	<div class="my-container banner pt-0">
+		<div class="banner-body radius">
+			<div class="sell-car-div text-center">
+				<p>Sell Your Car Today</p>
+				<p class="pt-3">Get a no-obligation quote and decide if selling today is right for you.</p>
+				<div class="input-grup pt-5">
+					<input type="text" class="bnr-text-input" placeholder="Enter your VIN or licence plate">
+					<!-- <button class="get-btn">Get Instant Offer</button> -->
+				</div>
+			</div>
+			 <div class="row">
+				<div class="col-3">
+					<div class="backside-card-1">
+						<div class="card-body-1">
+						 	<img src="img/Illustration.png">
+						 	<div>
+						 		<p>Invoice paid</p>
+						 		<p><b>$15,156.99</b></p>
+						 		<p>View invoice details</p>
+						 	</div>
+						 	<div class="invoice">
+						 		<div class="invoice-align">
+						 			<p>Invoice number</p>
+						 			<p>#526589</p>
+						 		</div>
+						 		<div class="invoice-align">
+						 			<p>Payment date</p>
+						 			<p>11 Jan 2024, 11:00 AM</p>
+						 		</div>
+						 		<div class="invoice-align">
+						 			<p>Payment method</p>
+						 			<p>**** 6787</p>
+						 		</div>
+						 	</div>
+						</div>
+					</div>
+					
+				</div>
+				<div class="col-6">2</div>
+				<div class="col-3">3</div>
+			 </div>
+		</div>
+	</div>
+</section>
